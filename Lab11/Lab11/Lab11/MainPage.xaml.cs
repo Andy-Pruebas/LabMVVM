09950233535ef3e720377e297a3e5f90d117abd6
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-06-09 19:01:33 -0500

    Primer commit by andy

---
+++ Lab11/Lab11/Lab11/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Lab11.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab11
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InteractiveDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SimpleDemo());
+
+            };
+        }
+    }
+}
